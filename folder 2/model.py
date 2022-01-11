127cad59ed4f0f40d79141a4343180728d30e4d2
Mohamed Anas <88653931+MdAnas5@users.noreply.github.com>
2022-01-11 10:27:03 +0530

    Add files via upload

---
+++ folder 2/model.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+# df = pd.read_csv('DayWiseResampleZero.csv', parse_dates=['SIHD_INV_DATE'])
+
+# df = pd.read_csv('WeekWiseResampleZero.csv', parse_dates=['SIHD_INV_DATE'])
+df = pd.read_csv('SM.csv')
+# df = pd.read_csv('MonthResampleZero.csv', parse_dates=['SIHD_INV_DATE'])
+
+# 1 Simple linear regression fit and prediction on time series data with visualization in python
+'''
+print(df.head())
+print(df.shape)
+x = np.arange(df['SIHD_INV_DATE'].size)
+# print(x)
+fit = np.polyfit(x, df['QTY'], deg=1)
+print(fit)
+print("Slope : " + str(fit[0]))
+print("Intercept : " + str(fit[1]))
+
+# Fit function : y = mx + c [linear regression ]
+fit_function = np.poly1d(fit)
+y = np.arange(df['SIHD_INV_DATE'].size+9)
+prediction = fit_function(y)
+prediction = pd.DataFrame(prediction)
+# prediction.to_csv('prediction.csv')
+print("Prediction", prediction)
+# plt.plot(prediction, 'r-', label='prediction')
+# plt.ylim(bottom=0, top=6500)
+# plt.xlim(left='30/03/2019', right='15/12/2021')
+# df = df.astype({'SIHD_INV_DATE': np.datetime64})
+# plt.plot(df['SIHD_INV_DATE'], df['QTY'], label='actual')
+# plt.xticks(rotation=90)
+'''
+# plt.ylim(bottom=0, top=6500)
+# plt.subplots_adjust(left=0.06, bottom=0.17, right=0.9, top=0.9)
+# plt.grid()
+#
+# # plt.xlim(left='30/03/2019', right='15/12/2021')
+# plt.ylim(bottom=0, top=6500)
+# plt.grid(True)
+# plt.show()
+df = df.astype({'SIHD_INV_DATE': np.datetime64})
+plt.plot(df['SIHD_INV_DATE'], df['QTY'], label='actual')
+plt.plot(df['SIHD_INV_DATE'], df['prediction'], label='predict')
+plt.ylim(bottom=0, top=6500)
+plt.xticks(rotation=90)
+plt.subplots_adjust(left=0.06, bottom=0.17, right=0.9, top=0.9)
+plt.grid(color='green', linestyle='--', linewidth=0.5)
+plt.show()
